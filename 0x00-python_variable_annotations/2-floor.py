5eb0a918d6223d60c5f33db84a531d55c640c93f
yahia008 <yahyatijjani99@gmail.com>
2024-08-01 15:46:47 +0000

    python

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module for Task 2."""
+import math
+
+
+def floor(n: float) -> int:
+    """Calculates the largest integer less than or
+    equal to a given floating-point number.
+    """
+    return math.floor(n)
